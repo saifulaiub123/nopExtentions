dae2b781c68ba9b63f34b021c1491db250f629e3
MD Saiful Islam <saifulprogrammer@gmail.com>
2024-10-23 13:56:09 +0600

    update

---
+++ Misc.NopHunter.LiveChat/SourceCode/Misc.NopHunter.LiveChat/4.00/Misc.NopHunter.LiveChat/LiveChatDefaults.cs
@@ -0,0 +1,19 @@
+﻿using Nop.Core;
+
+namespace Nop.Plugin.Misc.NopHunter.LiveChat;
+
+/// <summary>
+/// Represents plugin constants
+/// </summary>
+public class LiveChatDefaults
+{
+    /// <summary>
+    /// Gets a plugin system name
+    /// </summary>
+    public static string SystemName => "Misc.NopHunter.LiveChat";
+   
+    /// <summary>
+    /// Gets the configuration route name
+    /// </summary>
+    public static string ConfigurationRouteName => "Plugin.Misc.NopHunter.LiveChat.Configure";
+}
---
+++ Misc.NopHunter.LiveChat/SourceCode/Misc.NopHunter.LiveChat/4.40/Misc.NopHunter.LiveChat/LiveChatSettings.cs
@@ -0,0 +1,15 @@
+﻿using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Misc.NopHunter.LiveChat;
+
+/// <summary>
+/// Represents plugin settings
+/// </summary>
+public class LiveChatSettings : ISettings
+{
+    /// <summary>
+    /// Gets or sets the API key
+    /// </summary>
+    public string Script { get; set; }
+
+}
