5bbb564ea417a2dc1327c2f7cd623fb49c36fb48
MD Saiful Islam <saifulprogrammer@gmail.com>
2024-10-23 14:37:34 +0600

    update

---
+++ Misc.NopHunter.LiveChat/4.60/Misc.NopHunter.LiveChat/Controllers/LiveChatAdminController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Plugin.Misc.NopHunter.LiveChat.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Messages;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Framework.Mvc.Filters;
+
+namespace Nop.Plugin.Misc.NopHunter.LiveChat.Controllers;
+
+[Area(AreaNames.ADMIN)]
+[AuthorizeAdmin]
+[AutoValidateAntiforgeryToken]
+public class LiveChatAdminController : BasePluginController
+{
+    #region Fields
+
+    private readonly ILocalizationService _localizationService;
+    private readonly INotificationService _notificationService;
+    private readonly ISettingService _settingService;
+    private readonly LiveChatSettings _liveChatSettings;
+
+    #endregion
+
+    #region Ctor
+
+    public LiveChatAdminController(ILocalizationService localizationService,
+        INotificationService notificationService,
+        ISettingService settingService,
+        LiveChatSettings liveChatSettings)
+    {
+        _localizationService = localizationService;
+        _notificationService = notificationService;
+        _settingService = settingService;
+        _liveChatSettings = liveChatSettings;
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    public async Task<IActionResult> Configure()
+    {
+        var model = new ConfigurationModel
+        {
+            Script = _liveChatSettings.Script,
+        };
+
+        return View("~/Plugins/Misc.NopHunter.LiveChat/Views/Configure.cshtml", model);
+    }
+
+    [HttpPost, ActionName("Configure")]
+    [FormValueRequired("save")]
+    public async Task<IActionResult> Configure(ConfigurationModel model)
+    {
+        if (!ModelState.IsValid)
+            return await Configure();
+
+        _liveChatSettings.Script = model.Script;
+
+        await _settingService.SaveSettingAsync(_liveChatSettings);
+        _notificationService.SuccessNotification(await _localizationService.GetResourceAsync("Plugins.Misc.NopHunter.Common.UpdateSuccess"));
+        return await Configure();
+    }
+    #endregion
+}
