ceb2e5054c6f84ab373bea75678a22fdf881d2be
MD Saiful Islam <saifulprogrammer@gmail.com>
2024-10-25 19:37:33 +0600

    update

---
+++ Misc.NopHunter.LiveChat/4.60/Nop.Plugin.Misc.NopHunter.LiveChat/Infrastructure/RouteProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Nop.Web.Framework.Mvc.Routing;
+using Nop.Web.Framework;
+
+namespace Nop.Plugin.Misc.NopHunter.LiveChat.Infrastructure;
+
+/// <summary>
+/// Represents plugin route provider
+/// </summary>
+public class RouteProvider : IRouteProvider
+{
+    /// <summary>
+    /// Register routes
+    /// </summary>
+    /// <param name="endpointRouteBuilder">Route builder</param>
+    public void RegisterRoutes(IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        endpointRouteBuilder.MapControllerRoute(name: LiveChatDefaults.ConfigurationRouteName,
+            pattern: "Admin/LiveChat/Configure",
+            defaults: new { controller = "LiveChatAdmin", action = "Configure", area = AreaNames.Admin });
+    }
+
+    /// <summary>
+    /// Gets a priority of route provider
+    /// </summary>
+    public int Priority => 0;
+}
